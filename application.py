314a5097ae1c26428e8cfaf2414c33f224314ad4
Dilip Panda <dilipkupanda23@gmail.com>
2023-04-06 22:12:13 +0530

    changes in application

--- application.py
+++ application.py
@@ -85,10 +85,6 @@ def index():
 
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001, debug=True)
-	app.run(host='0.0.0.0',debug=True)
+	app.run(,debug=True)
 
 
-
-if __name__ == "__main__":
-
-	app.run(host='0.0.0.0',debug=True)
